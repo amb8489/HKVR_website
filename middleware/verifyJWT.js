d6232a6b158fc5d88616d98af28b9e4f883a7ad1
Aaron Berghash <amb8489@rit.edu>
2023-09-05 14:32:48 -0400

    updated backend

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,21 +1,24 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken')
 
 const verifyJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization || req.headers.Authorization;
-  // need a proper auth Header
-  if (!authHeader?.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
+    const authHeader = req.headers.authorization || req.headers.Authorization
 
-  // get the token from header
-  const token = authHeader.split(" ")[1];
+    if (!authHeader?.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Unauthorized' })
+    }
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.status(403).json({ message: "Forbidden" });
-    req.user = decoded.UserInfo.username;
-    req.roles = decoded.UserInfo.roles;
-    next();
-  });
-};
+    const token = authHeader.split(' ')[1]
 
-module.exports = verifyJWT;
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, decoded) => {
+            if (err) return res.status(403).json({ message: 'Forbidden' })
+            req.user = decoded.UserInfo.username
+            req.roles = decoded.UserInfo.roles
+            next()
+        }
+    )
+}
+
+module.exports = verifyJWT 
--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,17 +1,14 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const notesController = require('../controllers/notesController')
+const verifyJWT = require('../middleware/verifyJWT')
 
-// where functions for get post update delete are
-const notesController = require("../controllers/notesController");
-const verifyJWT = require("../middleware/verifyJWT");
+router.use(verifyJWT)
 
-router.use(verifyJWT);
+router.route('/')
+    .get(notesController.getAllNotes)
+    .post(notesController.createNewNote)
+    .patch(notesController.updateNote)
+    .delete(notesController.deleteNote)
 
-router
-  .route("/")
-  .get(notesController.getAllNotes)
-  .post(notesController.createNewNote)
-  .patch(notesController.updateNote)
-  .delete(notesController.deleteNote);
-
-module.exports = router;
+module.exports = router
